1f42a47e0447096f7c3c237334dbc4e519fa538f
jose blois <joseblois3@gmail.com>
2020-04-18 02:59:31 -0300

    can reset and start also score is shown after counter goes is 0

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ var mousex =0,mousey=0,x=0,y=0;
 var player = new Circle(x,y,15);
 var target = new Circle (100,100,20)
 var score = 0;
-var bgColor=null;
+var bgColor='#0f0';
 var img = new Image();
+var counter=15;
+var lastUpdate=null;
+var pause = true;
+var gameover=true;
+function reset(){
+    score=0;
+    counter=15;
+    gameover=false;
+    pause=true;
+}
 function init (){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -13,7 +23,6 @@ function init (){
     canvas.height=200;
     //declaring the images
     img.src='assets/img.png'
-
     //run and repaint functions
     enableInputs();
     run();
@@ -37,8 +46,9 @@ Circle.prototype.distance=function(circle){
         return(Math.sqrt(dx*dx+dy*dy)-(this.radius+circle.radius));}
     };
 Circle.prototype.drawImage = function (ctx,img){
-    ctx.drawImage(img,this.x-this.radius,this.y-this.radius,this.radius*2,this.radius*2);
-
+    if(img.width){
+   ctx.drawImage(img,this.x-this.radius,this.y-this.radius,this.radius*2,this.radius*2);
+    }else this.stroke(ctx)
 }
 //Mouse detection
 function enableInputs(){
@@ -54,7 +64,13 @@ document.addEventListener('mousedown',function(evt){
 function run(){
     window.requestAnimationFrame(run);
     //console.log('+1')
-    act();
+    var now = Date.now()
+    var deltaTime = (now-lastUpdate)/1000;
+    if(deltaTime>1){
+        deltaTime=0;
+    }
+    lastUpdate=now;
+    act(deltaTime);
     paint(ctx)
 }
 //repaint
@@ -69,38 +85,76 @@ function paint(ctx){
     ctx.textAlign='left'
     // ctx.fillText('X: '+player.x,0,20);
     // ctx.fillText('Y: '+player.y,0,30);
-    ctx.fillText('Distance: '+player.distance(target).toFixed(1),0,10)
-    ctx.fillText('Lastpress: '+lastPress,0,40);
-    ctx.fillText('Score: '+score,0,20)
-    lastPress=null;
     ctx.strokeStyle=bgColor;
     player.draw(ctx);
     target.drawImage(ctx,img)
+    ctx.font='10px sans-serif'
+    ctx.fillText('Distance: '+player.distance(target).toFixed(1),0,10)
+    ctx.fillText('Lastpress: '+lastPress,0,40);
+    ctx.fillText('Time: '+counter.toFixed(0),0,30);
+    ctx.fillText('Score: '+score,0,20)
+    lastPress=null;
+
+    if(pause){
+        ctx.font='20px sans-serif'
+        if(gameover){
+            ctx.textAlign='center';
+            ctx.fillText('Click to reset',canvas.width/2,canvas.height/2-50);
+            ctx.fillText('Score: '+score,canvas.width/2,canvas.height/2-30);
+        }else{
+            ctx.textAlign='center';
+            ctx.fillText('Click to start',canvas.width/2,canvas.height/2);
+        }
+    }
 }
-function act(){
-    player.x=mousex;
-    player.y=mousey;
-    if(player.x<0){
+function act(deltaTime){
+        player.x=mousex;
+        player.y=mousey;
+         
+            //  if(gameover){
+            //      reset()
+            //  }
+        if(player.x<0){
         player.x=0
-    }
-    if(player.x > canvas.width){
+        }
+        if(player.x > canvas.width){
         player.x=canvas.width
-    }
-    if(player.y<0){
+        }
+        if(player.y<0){
         player.y=0;
-    }
-    if(player.y>canvas.height){
+        }
+        if(player.y>canvas.height){
         player.y=canvas.height
-    }
-    if(lastPress===1){
-        bgColor ='#F00';
-        if(player.distance(target)<0){
+        }
+    if(!pause){
+        counter-=deltaTime;
+        if(counter<=0){
+            gameover=true;
+            pause=true;
+            counter=0;
+        }
+        if(lastPress===1){
+            bgColor ='#F00';
+            if(player.distance(target)<0){
             score++;
             target.x= random(canvas.width/10)*10+target.radius;
             target.y= random(canvas.height/10)*10+target.radius;
         }
-    }else{
+        }else{
         bgColor='#0f0'
+        }
+    }else if(lastPress===1){
+        if (gameover){
+            gameover=false;
+            counter=15;
+            score=0;
+            target.x=canvas.width/2
+            target.y=canvas.height/2
+        }else {pause=false;
+        lastPress=null;
+        target.x=random(canvas.width/10)*10+target.radius;
+        target.y=random(canvas.height/10)*10+target.radius;
+        }
     }
 }
 function random(max){
